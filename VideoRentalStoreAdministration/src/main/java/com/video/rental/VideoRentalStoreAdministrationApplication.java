3cf08a654e3879f738e07cbede6a03222c91dccd
administrator <administrator@gmg10>
2024-10-25 17:35:00 +0200

    Commit Inicial con la configuración del proyecto y la conexión a la base de datos

---
+++ VideoRentalStoreAdministration/src/main/java/com/video/rental/VideoRentalStoreAdministrationApplication.java
@@ -0,0 +1,13 @@
+package com.video.rental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoRentalStoreAdministrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoRentalStoreAdministrationApplication.class, args);
+	}
+
+}
